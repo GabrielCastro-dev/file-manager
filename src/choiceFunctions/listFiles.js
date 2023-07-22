59720f230497a60ce0819ef8eca9dab6d4a6deda
gabriel <gabrielcastro.developer@gmail.com>
2023-07-22 12:10:02 -0300

    removing files

--- src/choiceFunctions/listFiles.js
+++ src/choiceFunctions/listFiles.js
@@ -1,13 +1,35 @@
 import { readdirSync } from 'fs';
-import { fcGreen, Reset } from '../utils/ansiColors.js';
+import {
+    Reset,
+    fcBlue,
+    fcCyan,
+    fcYellow,
+    fcRed
+} from '../utils/ansiColors.js';
 
 function listFiles(directory){
     const files = readdirSync(directory);
 
-    console.log(fcGreen + '\nFiles whithin this folder: \n' + Reset);
+    console.log(fcBlue + '\nContent whithin this folder: \n' + Reset);
 
     files.forEach(file => {
-        console.log(file);
+        const isFile = file.includes('.');
+
+        if(isFile){
+            const separateByDot =  file.split('.');
+            const addANSIColors = (word) => {
+                if (word === 'js') return fcYellow + word + Reset;
+                if (word === 'rb') return fcRed + word + Reset;
+                if (word === 'py') return fcBlue + word + Reset;
+                return word; // Return the word unchanged if no color is specified
+            };
+
+              const fileName = separateByDot.map(addANSIColors).join('.');
+              console.log('File: ' + fileName);
+        } else {
+            console.log('Folder: ' + fcCyan + file + Reset);
+        }
+
     });
 
     console.log('');
