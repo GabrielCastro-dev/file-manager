bf591acd22b948152cd943375e72313e0b83ee36
gabriel <gabrielcastro.developer@gmail.com>
2023-07-21 23:07:39 -0300

    menu colors

--- src/displayMenu.js
+++ src/displayMenu.js
@@ -1,16 +1,18 @@
+import { fcBlue, Reset} from './utils/ansiColors.js';
+
 function displayMenu(directory){
-    console.log(`\nWelcome to Interactive File Manager!\n`);
+    console.log(`${fcBlue}\nWelcome to the Interactive File Manager!\n${Reset}`);
     console.log(`Current Directory: ${directory}\n`);
-    console.log(`1. List Files`);
-    console.log(`2. Navigate to a Folder`);
-    console.log(`3. Create a New File`);
-    console.log(`4. Create a New Folder`);
-    console.log(`5. Delete a File/Folder`);
-    console.log(`6. Move/Rename a File/Folder`);
-    console.log(`7. Edit a Text File`);
-    console.log(`8. Search Files`);
-    console.log(`9. Show menu again`);
-    console.log(`10. Exit\n`);
+    console.log(`${fcBlue}1.${Reset} List Files`);
+    console.log(`${fcBlue}2.${Reset} Navigate to a Folder`);
+    console.log(`${fcBlue}3.${Reset} Create a New File`);
+    console.log(`${fcBlue}4.${Reset} Create a New Folder`);
+    console.log(`${fcBlue}5.${Reset} Delete a File/Folder`);
+    console.log(`${fcBlue}6.${Reset} Move/Rename a File/Folder`);
+    console.log(`${fcBlue}7.${Reset} Edit a Text File`);
+    console.log(`${fcBlue}8.${Reset} Search Files`);
+    console.log(`${fcBlue}9.${Reset} Show menu again`);
+    console.log(`${fcBlue}10${Reset}. Exit\n`);
 }
 
 export default displayMenu;
