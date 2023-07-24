946ade4cd0f25e35ea1a2a18d95992e9ab45c583
gabriel <gabrielcastro.developer@gmail.com>
2023-07-24 20:46:05 -0300

    navigate to external folder

--- src/choiceFunctions/changeDirectory.js
+++ src/choiceFunctions/changeDirectory.js
@@ -5,9 +5,19 @@ import { existsSync } from 'fs';
 import inputValidation from '../validations/inputValidation.js';
 
 function changeDirectory(directory){
+    const isSubfolder = prompt('The directory you want to navigate to is a subfolder (y/n)? ');
+
+    // User's isSubfolder input validation
+    const validatedSubfolderInput = inputValidation(isSubfolder);
+
+    if(!validatedSubfolderInput.isInputValid){
+        console.log(validatedSubfolderInput.message);
+        return;
+    }
+
     const userInput = prompt('Enter the destination: ');
 
-    // User's input validation
+    // User's destination input validation
     const validatedInput = inputValidation(userInput);
 
     if(!validatedInput.isInputValid){
@@ -15,17 +25,30 @@ function changeDirectory(directory){
         return;
     }
 
-    // Directory existence verification
-    const newDirectory = directory.value + userInput + '/';
-    const folderExists = existsSync(newDirectory);
+    // Checks if the directory is a subfolder
+    switch (isSubfolder) {
+        case 'y':
+            // Directory existence verification
+            const newDirectory = directory.value + userInput + '/';
+            const folderExists = existsSync(newDirectory);
 
-    if(!folderExists){
-        console.log(`\n${fcRed}This folder doesn't exists! ${Reset}`);
-        return;
+            if(!folderExists){
+                console.log(`\n${fcRed}This folder doesn't exists! ${Reset}`);
+                return;
+            }
+
+            // Sets new directory 
+            directory.value =  newDirectory;
+            console.log('');
+            break;
+        case 'n':
+            directory.value = userInput + '/';
+            console.log('');
+            break;
+        default:
+            console.log(fcRed + 'Please enter a valid value.' + Reset);
+            break;
     }
-
-    directory.value =  newDirectory;
-    console.log('');
 }
 
 export default changeDirectory;
